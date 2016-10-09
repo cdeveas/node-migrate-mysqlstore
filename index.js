8a91aff697bf7f097d1760371d37734dc19bc356
Chas DeVeas <chas@videoblocks.com>
2016-10-09 12:50:24 +0200

    Updated project to use standardjs.

--- index.js
+++ index.js
@@ -1,134 +1,136 @@
-'use strict';
+'use strict'
 
-var mysql = require('mysql');
+var mysql = require('mysql')
 
-module.exports = MysqlStore;
+module.exports = MysqlStore
 
 /**
  * @param {object} options
  * @constructor
  */
-function MysqlStore(options) {
-    this.host = options.host || '127.0.0.1';
-    this.port = options.port || 3306;
-    this.user = options.user || 'root';
-    this.password = options.password || 'password';
-    this.database = options.database || 'migrations';
-    this.migrationsTable = options.migrationsTable || 'migrations';
+function MysqlStore (options) {
+  this.host = options.host || '127.0.0.1'
+  this.port = options.port || 3306
+  this.user = options.user || 'root'
+  this.password = options.password || 'password'
+  this.database = options.database || 'migrations'
+  this.migrationsTable = options.migrationsTable || 'migrations'
 
-    // Initialize the migrations migrationsTable (this is async)
-    this.init(function(err) {
-        if (err) throw err;
-    });
+  // Initialize the migrations migrationsTable (this is async)
+  this.init(function (err) {
+    if (err) throw err
+  })
 }
 
 MysqlStore.cliHandler = {
-    usageOptions: [
-        '     --host <string>  Mysql server host',
-        '     --port <number>  Mysql server port',
-        '     --user <string>  Mysql user',
-        '     --password <string>  Mysql password',
-        '     --database <string>  Mysql database in which to save migration executions',
-        '     --migrationsTable <string>  Mysql table in which to save migration executions'
-    ],
-    parseArg: function(arg) {
-        switch (arg) {
-            case '--host':
-                return 'host';
-            case '--port':
-                return 'port';
-            case '--user':
-                return 'user';
-            case '--password':
-                return 'password';
-            case '--database':
-                return 'database';
-            case '--migrationsTable':
-                return 'migrationsTable';
-        }
+  usageOptions: [
+    '     --host <string>  Mysql server host',
+    '     --port <number>  Mysql server port',
+    '     --user <string>  Mysql user',
+    '     --password <string>  Mysql password',
+    '     --database <string>  Mysql database in which to save migration executions',
+    '     --migrationsTable <string>  Mysql table in which to save migration executions'
+  ],
+  parseArg: function (arg) {
+    switch (arg) {
+      case '--host':
+        return 'host'
+      case '--port':
+        return 'port'
+      case '--user':
+        return 'user'
+      case '--password':
+        return 'password'
+      case '--database':
+        return 'database'
+      case '--migrationsTable':
+        return 'migrationsTable'
     }
-};
+  }
+}
 
-MysqlStore.prototype._getConnection = function(callback) {
+MysqlStore.prototype._getConnection = function (callback) {
+  var connection
+  try {
+    connection = mysql.createConnection({
+      host: this.host,
+      database: this.database,
+      port: this.port,
+      user: this.user,
+      password: this.password
+    })
+  } catch (e) {
+    callback(e)
+  }
 
-    var connection;
-    try {
-        connection = mysql.createConnection({
-            host: this.host,
-            database: this.database,
-            port: this.port,
-            user: this.user,
-            password: this.password
-        });
-    } catch (e) {
-        callback(e);
-    }
+  callback(null, connection)
+}
 
-    callback(null, connection);
-};
+MysqlStore.prototype.load = function (callback) {
+  var self = this
+  this.lock(function (err, conn) {
+    if (err) return callback(err)
 
-MysqlStore.prototype.load = function(callback) {
-    var self = this;
-    this.lock(function(err, conn) {
-        if (err) return callback(err);
-
-        // then search for an existing title sorted by timestamp ASC, return array of results
-        var sql = '\
+    // then search for an existing title sorted by timestamp ASC, return array of results
+    var sql = '\
 SELECT title, operation, timestamp \
 FROM ' + self.migrationsTable + ' \
 ORDER BY timestamp ASC \
-';
-        conn.query(sql, function(err, results) {
-            callback(err, results);
-            conn.end();
-        });
-    });
-};
+'
+    conn.query(sql, function (err, results) {
+      callback(err, results)
+      conn.end()
+    })
+  })
+}
 
-MysqlStore.prototype.save = function(migration, callback) {
-    var self = this;
-    return this._getConnection(function(err, conn) {
-        if (err) return callback(err);
-        var sql = '\
+MysqlStore.prototype.save = function (migration, callback) {
+  var self = this
+  return this._getConnection(function (err, conn) {
+    if (err) return callback(err)
+    var sql = '\
 INSERT INTO ' + self.migrationsTable + ' (title, operation, timestamp) \
-VALUES (' + [migration.title, migration.operation, migration.timestamp].map(_escape).join(',') + ')';
-        _queryThenEnd(conn, sql, callback);
-    });
-};
+VALUES (' + [ migration.title, migration.operation, migration.timestamp ].map(_escape).join(',') + ')'
+    _queryThenEnd(conn, sql, callback)
+  })
+}
 
-MysqlStore.prototype.lock = function(callback) {
-    var self = this;
-    this.init(function(err, conn) {
-        if (err)  return callback(err);
-        var sql = 'LOCK TABLES ' + self.migrationsTable + ' READ';
-        conn.query(sql, function(err) {
-            callback(err, conn); // callback with connection so we can use it without ending it
-        });
-    });
-};
+MysqlStore.prototype.lock = function (callback) {
+  var self = this
+  this.init(function (err, conn) {
+    if (err) return callback(err)
+    var sql = 'LOCK TABLES ' + self.migrationsTable + ' READ'
+    conn.query(sql, function (err) {
+      callback(err, conn) // callback with connection so we can use it without ending it
+    })
+  })
+}
 
-MysqlStore.prototype.unlock = function(callback) {
-    this._getConnection(function(err, conn) {
-        if (err)  return callback(err);
-        var sql = 'UNLOCK TABLES';
-        _queryThenEnd(conn, sql, callback)
-    });
-};
+MysqlStore.prototype.unlock = function (callback) {
+  this._getConnection(function (err, conn) {
+    if (err) return callback(err)
+    var sql = 'UNLOCK TABLES'
+    _queryThenEnd(conn, sql, callback)
+  })
+}
 
-MysqlStore.prototype.reset = function(callback) {
-    var self = this;
-    self._getConnection(function(err, conn) {
-        var sql = '\
+MysqlStore.prototype.reset = function (callback) {
+  var self = this
+  self._getConnection(function (err, conn) {
+    if (err) return callback(err)
+    var sql = '\
 DROP TABLE ' + self.migrationsTable + ' \
-';
-        _queryThenEnd(conn, sql, callback);
-    });
-};
+'
+    _queryThenEnd(conn, sql, callback)
+  })
+}
 
-MysqlStore.prototype.init = function(callback) {
-    var self = this;
-    self._getConnection(function(err, conn) {
-        var sql = '\
+MysqlStore.prototype.init = function (callback) {
+  var self = this
+  self._getConnection(function (err, conn) {
+    if (err) return callback(err)
+
+    var sql = '\
 CREATE TABLE IF NOT EXISTS ' + self.migrationsTable + ' (\
     `id` INT NOT NULL AUTO_INCREMENT, \
     `title` VARCHAR(255) NOT NULL, \
@@ -137,20 +139,20 @@ CREATE TABLE IF NOT EXISTS ' + self.migrationsTable + ' (\
     PRIMARY KEY (`id`)\
 )\
 ENGINE = InnoDB\
-';
-        conn.query(sql, function(err) {
-            callback(err, conn);
-        });
-    });
-};
-
-function _queryThenEnd(connection, sql, callback) {
-    connection.query(sql, function(err) {
-        if (err) return callback(err);
-        connection.end(callback);
+'
+    conn.query(sql, function (err) {
+      callback(err, conn)
     })
+  })
 }
 
-function _escape(val) {
-    return mysql.escape(val);
+function _queryThenEnd (connection, sql, callback) {
+  connection.query(sql, function (err) {
+    if (err) return callback(err)
+    connection.end(callback)
+  })
+}
+
+function _escape (val) {
+  return mysql.escape(val)
 }
